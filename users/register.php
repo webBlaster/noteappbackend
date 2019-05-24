ca3d6bf65d456a26c31d23afca986133cf0ee883
Yekinni Ajibola <yekinnijibola@gmail.com>
2019-05-24 22:09:32 +0100

    got read of issets

--- users/register.php
+++ users/register.php
@@ -2,8 +2,8 @@
 require('../objects/user.php');
 //get variables 
 //sanitize variables
-$name = htmlentities(isset($_POST['username']));
-$email =htmlentities(isset($_POST['email']));
-$password = md5(isset($_POST['password']));
+$name = htmlentities($_POST['username']);
+$email =htmlentities($_POST['email']);
+$password = md5($_POST['password']);
 //register
 $user->register($name,$email,$password);
